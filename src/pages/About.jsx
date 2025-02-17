4cba763fdf7f431185e37fb9aadbdd38d885ec46
Toby Woollums <woollumst@gmail.com>
2025-02-16 21:55:33 -0700

    fixed second p tag

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,11 +3,7 @@ function About() {
         <div className="about">
             <h1>About Me</h1>
             <h2>Bio</h2>
-            <p>Hi, I'm Toby Woollums, a passionate aspiring software developer working towards becoming a 
-        **full-stack developer**. I have a strong foundation in **Java and JavaScript** and am 
-        expanding my expertise in **React, TypeScript, and DevOps tools**.  
-        Currently, I'm pursuing a **B.S. in Computer Science at WGU** while actively working on 
-        projects to improve my skills</p>
+            <p>Hi, I'm Toby Woollums, a passionate aspiring software developer working towards becoming a **full-stack developer**. I have a strong foundation in **Java and JavaScript** and am expanding my expertise in **React, TypeScript, and DevOps tools**. Currently, I'm pursuing a **B.S. in Computer Science at WGU** while actively working on projects to improve my skills</p>
             <br />
             <h2>Skills</h2>
             <p>Languages: Java, C++, SQL, JavaScript, TypeScript (Learning)</p>
@@ -15,7 +11,7 @@ function About() {
             <p>Back End: Spring (Java), Node.js</p>
             <p>Dev Ops: Docker, AWS (Learning)</p>
             <p>Version Control: Git @ GitHub</p>
-            <p>Testing: JUnit (TDD), Mockito
+            <p>Testing: JUnit (TDD), Mockito</p>
             <br />
             <h2>Education & Certifications</h2>
             <h3>B.S. Computer Science (In Progress)</h3>
@@ -29,9 +25,7 @@ function About() {
             <p>Revature | 08/2024 - Present</p>
             <br />
             <h2>Future Goals</h2>
-            <p>My goal is to land my first **software development role** while continuing to grow in 
-        **React, full-stack development, and DevOps**. I'm eager to work on meaningful projects 
-        and contribute to a dynamic engineering team.</p>
+            <p>My goal is to land my first **software development role** while continuing to grow in **React, full-stack development, and DevOps**. I'm eager to work on meaningful projects and contribute to a dynamic engineering team.</p>
         </div>
     );
 }
