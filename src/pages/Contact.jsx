3d12f2f970c9f4f9a1889f8cdba9959304f28cbf
Toby <woollumst@gmail.com>
2025-02-16 21:13:50 -0700

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,14 @@
 function Contact () {
     return (
         <div className="contact">
-            <p>contact page</p>
+            <h1>Contact Me</h1>
+            <p>Feel free to reach out if you'd like to connect!</p>
+            <br>
+            <h2>Toby Woollums</h2>
+            <h3>Software Developer</h3>
+            <p><a href="mailto:woollumst@gmail.com" target="_blank">Email: woollumst@gmail.com</a></p>
+            <p><a href="https://www.linkedin.com/in/toby-woollums" target="_blank">LinkedIn</a></p>
+            <p><a href="https://github.com/woollumst/" target="_blank">Github</a></p>
         </div>
     );
 }
