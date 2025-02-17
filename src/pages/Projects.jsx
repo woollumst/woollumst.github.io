f760775d16aa3d678ee4aa3f2dd4debe413782f1
Toby <woollumst@gmail.com>
2025-02-16 22:40:29 -0700

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,20 +4,20 @@ function Projects () {
     return (
         <div className="projects">
             <h1>My Projects</h1>
-            <ul>
-                <li>
+            <ul className="project-list">
+                <li className="project-list-item">
                     <h2>Employee Management System</h2>
                     <p>Language: Java (Spring), JavaScript (React)</p>
                     <a href="https://www.github.com/woollumst/EmployeeManagementSystem/" target="_blank">Link to GitHub Repo</a>
                     <p>Description: sample text</p>
                 </li>
-                <li>
+                <li className="project-list-item">
                     <h2>Social Media API (Spring)</h2>
                     <p>Language: Java (Spring)</p>
                     <a href="https://www.github.com/woollumst/woollumst-pep-spring-project/" target="_blank">Link to GitHub Repo</a>
                     <p>Description: sample text</p>
                 </li>
-                <li>
+                <li className="project-list-item">
                     <h2>Student System</h2>
                     <p>Language: C++</p>
                     <a href="https://www.github.com/woollumst/StudentSystem/" target="_blank">Link to GitHub Repo</a>
