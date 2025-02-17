4d8250d2a1f2916c917a2588df17b3e417173828
Toby <woollumst@gmail.com>
2025-02-16 21:25:42 -0700

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,7 @@ function Contact () {
         <div className="contact">
             <h1>Contact Me</h1>
             <p>Feel free to reach out if you'd like to connect!</p>
-            <br>
+            <br />
             <h2>Toby Woollums</h2>
             <h3>Software Developer</h3>
             <div className="contact-links">
