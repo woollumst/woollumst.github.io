e528b0f61e5fa65d632c974eb6009d575adfc977
Toby <woollumst@gmail.com>
2025-02-16 20:26:02 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <h1>Toby Woollums</h1>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+      <div className="logo">My Portfolio</div>
+
+      {/* Hamburger Icon */}
+      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+        ☰
+      </div>
+
+      {/* Navigation Links */}
+      <ul className={isOpen ? "nav-links open" : "nav-links"}>
+        <li>
+          <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+        </li>
+        <li>
+          <Link to="/about" onClick={() => setIsOpen(false)}>About</Link>
+        </li>
+        <li>
+          <Link to="/projects" onClick={() => setIsOpen(false)}>Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
+        </li>
       </ul>
     </nav>
   );
